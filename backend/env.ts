69b432fcd8ab906dda1d13264cf67e9d0815b8b9
Daniel Marrara <marraradaniel@gmail.com>
2021-02-17 14:17:59 -0300

    Added migrations

--- backend/env.ts
+++ backend/env.ts
@@ -16,7 +16,7 @@ import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
   MYSQL_HOST: Env.schema.string({ format: 'host' }),
-  MYSQL_PORT: Env.schema.number({ format: 'port' }),
+  MYSQL_PORT: Env.schema.number(),
   MYSQL_USER: Env.schema.string(),
   MYSQL_PASSWORD: Env.schema.string.optional(),
   MYSQL_DB_NAME: Env.schema.string(),
